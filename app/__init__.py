a79dd9fa6d3f09742e0c7da01b3aa0361a02af25
Hannah McKenna <56832992+mckhannah1901@users.noreply.github.com>
2021-03-24 12:26:05 +0000

    Refactoring code out of the app.py file

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "cs4227"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new_blog_post.db'
+db = SQLAlchemy(app)
+
+from app.models import Person, Blogpost, Comment
+from app import routes
