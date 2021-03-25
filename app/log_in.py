c083e044f8763ea6ec10ffe48f4ec7fd5d2209d3
Thomas <thomas.moorhead.langley@gmail.com>
2021-03-25 14:50:39 +0000

    Separation of functionality for log_in.py

--- app/log_in.py
+++ app/log_in.py
@@ -8,7 +8,6 @@ def log_in(email, password):
     if user_login is not None:
         session['username'] = user_login.username
         session['id'] = user_login.id
-        print(session['username'], session['id'])
     else:
         print("Ensure a valid email/password is input!")
         raise Exception
