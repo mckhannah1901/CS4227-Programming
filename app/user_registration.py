aeec90f1bfa777b14b36b0427f8404e9893e47af
Thomas <thomas.moorhead.langley@gmail.com>
2021-03-25 14:29:16 +0000

    Separation of functionality for user_registration.py

--- app/user_registration.py
+++ app/user_registration.py
@@ -11,10 +11,13 @@ def register_new_user(first_name, last_name, email, username, password):
 
     if first_name == '' or last_name == '' or email == '' or username == '' or password == '':
         print("All fields of the form must be filled in!")
+        raise Exception
     elif email_exists:
         print("This email already exists in the database. Please choose another!")
+        raise Exception
     elif username_exists:
         print("This username already exists in the database. Please choose another!")
+        raise Exception
     else:
         db.session.add(registration)
         db.session.commit()
