fa9e07723aeef85e105d680e1ceabcba9f629cc1
Hannah McKenna <56832992+mckhannah1901@users.noreply.github.com>
2021-03-26 16:54:42 +0000

    User Subbing finally works :)

--- app/add_post.py
+++ app/add_post.py
@@ -7,8 +7,9 @@ from app import Blogpost, db
 
 def add_post(title, content):
     username = session['username']
+    person_id = session['id']
 
-    post = Blogpost(title=title, username=username, content=content, date=datetime.now())
+    post = Blogpost(title=title, username=username, content=content, date=datetime.now(), user_id=person_id)
 
     post_exists = db.session.query(Blogpost.id).filter_by(title=title).first()
 
--- app/models.py
+++ app/models.py
@@ -1,14 +1,46 @@
 from flask_restless import APIManager
-from app import db, app
+from app import db, app, login_manager
+from flask_login import UserMixin
+
+users_to_follow = db.Table(
+    'users_to_follow',
+    db.Column('user_following_id', db.Integer, db.ForeignKey('person.id')),
+    db.Column('user_being_followed_id', db.Integer, db.ForeignKey('person.id'))
+)
 
 
-class Person(db.Model):
+class Person(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
     email = db.Column(db.Text)
     username = db.Column(db.Text)
     password = db.Column(db.Text)
+    followed_user = db.relationship(
+        'Person', secondary=users_to_follow,
+        primaryjoin=(users_to_follow.c.user_following_id == id),
+        secondaryjoin=(users_to_follow.c.user_being_followed_id == id),
+        backref=db.backref('users_to_follow', lazy='dynamic'), lazy='dynamic')
+
+    def __repr__(self):
+        return '<Person {}>'.format(self.username)
+
+    def follow_user(self, person):
+        if not self.is_following_user(person):
+            self.followed_user.append(person)
+
+    def unfollow_user(self, person):
+        if self.is_following_user(person):
+            self.followed_user.remove(person)
+
+    def is_following_user(self, person):
+        return self.followed_user.filter(
+            users_to_follow.c.user_being_followed_id == person.id).count() > 0
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return Person.query.get(int(id))
 
 
 class Blogpost(db.Model):
@@ -17,6 +49,7 @@ class Blogpost(db.Model):
     username = db.Column(db.Text)
     date = db.Column(db.DateTime)
     content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('person.id'))
 
 
 class Comment(db.Model):
