ccabd7039e5c18064bbceea2a78b3400c72c00fa
Thomas <thomas.moorhead.langley@gmail.com>
2021-04-01 16:22:38 +0100

    Memento implementation.

--- app/edit_post.py
+++ app/edit_post.py
@@ -10,31 +10,40 @@ def edit_post(post_id):
     edit_caretaker = models.PostEditCaretaker()
 
     original_blog_post_data = Blogpost.query.filter_by(id=post_id)[0]
+    existing_blog_post_data = original_blog_post_data
     new_blog_post_data = get_blog_post_data(post_id)
 
     editor = models.PostEditUtility(original_blog_post_data)
 
     try:
-        original_blog_post_data.title = new_blog_post_data.title
-        original_blog_post_data.username = new_blog_post_data.username
-        original_blog_post_data.date = new_blog_post_data.date
-        original_blog_post_data.content = new_blog_post_data.content
+        existing_blog_post_data.title = new_blog_post_data.title
+        existing_blog_post_data.username = new_blog_post_data.username
+        existing_blog_post_data.date = new_blog_post_data.date
+        existing_blog_post_data.content = new_blog_post_data.content
 
         editor.edit(new_blog_post_data)
 
-        flag_modified(original_blog_post_data, "title")
-        flag_modified(original_blog_post_data, "date")
-        flag_modified(original_blog_post_data, "username")
-        flag_modified(original_blog_post_data, "content")
+        flag_modified(existing_blog_post_data, "title")
+        flag_modified(existing_blog_post_data, "date")
+        flag_modified(existing_blog_post_data, "username")
+        flag_modified(existing_blog_post_data, "content")
 
         edit_caretaker.save(editor)
 
-        db.session.merge(original_blog_post_data)
+        db.session.merge(existing_blog_post_data)
         db.session.flush()
         db.session.commit()
 
+        # edit_caretaker.undo(editor)
+        # db.session.merge(original_blog_post_data)
+        # db.session.flush()
+        # db.session.commit()
+
     except Exception as ex:
         edit_caretaker.undo(editor)
+        db.session.merge(original_blog_post_data)
+        db.session.flush()
+        db.session.commit()
 
 
 def get_blog_post_data(id):
