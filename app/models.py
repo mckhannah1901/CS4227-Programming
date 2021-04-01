d620a77b6f98d9c2743667e5c3f19622cdbca54f
Thomas <thomas.moorhead.langley@gmail.com>
2021-04-01 15:31:34 +0100

    Implementing Builder pattern for the registration of an account.

--- app/models.py
+++ app/models.py
@@ -9,6 +9,75 @@ users_to_follow = db.Table(
 )
 
 
+class PersonManager:
+    _builder = None
+
+    def set_builder(self, builder):
+        self._builder = builder
+
+    def get_person(self):
+        person = Person()
+
+        username = self._builder.get_username()
+        person.set_username(username)
+
+        email = self._builder.get_email()
+        person.set_email(email)
+
+        password = self._builder.get_password()
+        person.set_password(password)
+
+        first_name = self._builder.get_first_name()
+        person.set_first_name(first_name)
+
+        last_name = self._builder.get_last_name()
+        person.set_last_name(last_name)
+
+        return person
+
+
+class Builder:
+    def get_username(self): pass
+
+    def get_email(self): pass
+
+    def get_password(self): pass
+
+    def get_first_name(self): pass
+
+    def get_last_name(self): pass
+
+
+class PersonBuilder(Builder):
+    username = None
+    email = None
+    password = None
+    first_name = None
+    last_name = None
+
+    def set_variables(self, username, email, password, first_name, last_name):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+
+    def get_email(self):
+        return self.password
+
+    def get_first_name(self):
+        return self.first_name
+
+    def get_last_name(self):
+        return self.last_name
+
+
 class Person(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
@@ -21,6 +90,28 @@ class Person(UserMixin, db.Model):
         primaryjoin=(users_to_follow.c.user_following_id == id),
         secondaryjoin=(users_to_follow.c.user_being_followed_id == id),
         backref=db.backref('users_to_follow', lazy='dynamic'), lazy='dynamic')
+
+    def __init__(self):
+        self._username = None
+        self._email = None
+        self._password = None
+        self._first_name = None
+        self._last_name = None
+
+    def set_username(self, username):
+        self._username = username
+
+    def set_email(self, email):
+        self._email = email
+
+    def set_password(self, password):
+        self._password = password
+
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+
+    def set_last_name(self, last_name):
+        self._last_name = last_name
 
     def __repr__(self):
         return '<Person {}>'.format(self.username)
